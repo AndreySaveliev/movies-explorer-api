429e14ed0c8a81238fa2ace0dfc24e9cab7aecfe
AndreySaveliev <101524084+AndreySaveliev@users.noreply.github.com>
2022-11-25 19:07:06 +0300

    validate data before passing it to controller

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,13 @@
 const router = require('express').Router();
+const { Joi, celebrate } = require('celebrate');
 const { getMe, changeUserInfo } = require('../controllers/user');
 
 router.get('/me', getMe);
-router.patch('/me', changeUserInfo);
+router.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    email: Joi.string().email(),
+  }),
+}), changeUserInfo);
 
 module.exports = router;
