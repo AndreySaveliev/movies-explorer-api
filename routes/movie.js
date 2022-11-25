657ae57909fa2874e210c63224dc4858748917a0
AndreySaveliev <101524084+AndreySaveliev@users.noreply.github.com>
2022-11-25 19:07:31 +0300

    changes in validation methods

--- routes/movie.js
+++ routes/movie.js
@@ -10,7 +10,7 @@ router.post(
       country: Joi.string().required(),
       director: Joi.string().required(),
       duration: Joi.number().required(),
-      year: Joi.number().required(),
+      year: Joi.string().required(),
       description: Joi.string().required(),
       image: Joi.string()
         .required()
@@ -23,7 +23,7 @@ router.post(
         .pattern(/https?:\/\/(www\.)?[-a-zA-Z0-9]{1,256}\.[a-zA-Z0-9]{1,6}[a-zA-Z0-9._~:/?#[\]@!$&()*+,;=-]{1,256}$/),
       nameRU: Joi.string().required(),
       nameEN: Joi.string().required(),
-      movieId: Joi.string().required(),
+      movieId: Joi.number().required(),
     }),
   }),
   postMovie,
@@ -32,7 +32,7 @@ router.delete(
   '/:movieId',
   celebrate({
     params: Joi.object().keys({
-      movieId: Joi.string().required().alphanum().hex(),
+      movieId: Joi.string().required().hex().length(24),
     }),
   }),
   deleteMovie,
