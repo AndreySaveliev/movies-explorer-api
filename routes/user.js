b925ed0040015d5448c9f2b699635e6eb72e8d2a
AndreySaveliev <101524084+AndreySaveliev@users.noreply.github.com>
2022-11-26 15:20:23 +0300

    add new avlidation rules before passing data to controller

--- routes/user.js
+++ routes/user.js
@@ -5,8 +5,8 @@ const { getMe, changeUserInfo } = require('../controllers/user');
 router.get('/me', getMe);
 router.patch('/me', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    email: Joi.string().email(),
+    name: Joi.string().min(2).max(30).required(),
+    email: Joi.string().email().required(),
   }),
 }), changeUserInfo);
 
